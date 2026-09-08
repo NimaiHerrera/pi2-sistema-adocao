3a76c08b48108233a265ec61b61fb5f83d0e5da2
NimaiH <nimai.senemo@hotmail.com>
2026-09-07 21:22:44 -0300

    feat: configuracao inicial da API FastAPI com endpoint Hello World

---
+++ backend/app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="API Comparador de Preços")
+
+@app.get("/")
+def home():
+    return {
+        "mensagem": "API do Projeto Integrador II - Univesp",
+        "status": "Online"
+    }
